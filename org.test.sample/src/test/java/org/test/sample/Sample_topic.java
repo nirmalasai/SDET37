28916c9e9e3c17f4d22aa59b00d9d81041e5737c
sakhi <sakhi@Nirmala>
2022-07-20 12:43:40 +0530

    New commit2

---
+++ org.test.sample/src/test/java/org/test/sample/Sample_topic.java
@@ -0,0 +1,12 @@
+package org.test.sample;
+
+import org.testng.annotations.Test;
+
+public class Sample_topic {
+	@Test
+	public void demo1()
+	{
+		System.out.println("testing application");
+	}
+
+}
